1cea59dcb2b504ef9a07a9f980a443e2039ca222
Gurifon <94828462+Gurifon@users.noreply.github.com>
2022-11-29 10:56:27 +0100

    Equip finish

--- Assets/Scripts/Item/ArmorScript.cs
+++ Assets/Scripts/Item/ArmorScript.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 public class ArmorScript : MonoBehaviour, IEquipable
 {
-    public Image _image;
     public Sprite _ArmorSprite;
     public void Equipable()
     {
+        Image  _image = GameObject.Find("Canvas").transform.GetChild(0).GetChild(4).GetChild(2).GetComponent<Image>();
         Debug.Log("Equip");
-        _image.sprite = _ArmorSprite; 
+        _image.sprite = _ArmorSprite ; 
     }
 }
